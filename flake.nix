2697cc34a52843ed2f38d14268501858e2c5e4e2
Nick Cao <nickcao@nichi.co>
2022-09-08 19:45:35 +0800

    add devshell

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NickCao/nixpkgs/nixos-unstable-small";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; }; in
+        with pkgs; rec {
+          devShells.default = mkShell {
+            nativeBuildInputs = [ cargo rustc rust-analyzer rustfmt clippy iperf3 ];
+          };
+        }
+      );
+}
