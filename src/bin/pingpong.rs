d7ba0387518865bb0f555115e344bfd5d9a8a3c7
Nick Cao <nickcao@nichi.co>
2022-09-11 23:06:11 +0800

    restore multi threading

--- src/bin/pingpong.rs
+++ src/bin/pingpong.rs
@@ -11,7 +11,7 @@ use std::net::SocketAddr;
 use std::net::UdpSocket;
 use std::os::unix::prelude::AsRawFd;
 use std::slice::{from_raw_parts, from_raw_parts_mut};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use x25519_dalek::{PublicKey, StaticSecret};
 
 const PKT_SIZE: usize = 65535;
@@ -79,91 +79,98 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sk = StaticSecret::from([1u8; 32]);
     let pk = PublicKey::from(&sk);
     let limiter = Arc::new(RateLimiter::new(&pk, 100));
-    let mut tunnel = Tunn::new(sk, pk, None, None, 0, limiter).unwrap(); // FIXME: use peer pk
+    let tunnel = Arc::new(Mutex::new(
+        Tunn::new(sk, pk, None, None, 0, limiter).unwrap(),
+    )); // FIXME: use peer pk
 
-    let eventfd = unsafe { libc::eventfd(0, libc::EFD_CLOEXEC | libc::EFD_NONBLOCK) };
+    for _ in 0..4 {
+        let tunnel = tunnel.clone();
+        std::thread::spawn(move || {
+            let eventfd = unsafe { libc::eventfd(0, libc::EFD_CLOEXEC | libc::EFD_NONBLOCK) };
 
-    let mut poll = Poll::new().unwrap();
-    poll.registry()
-        .register(&mut SourceFd(&eventfd), Token(0), Interest::READABLE)
-        .unwrap();
-    let mut events = Events::with_capacity(1024);
+            let mut poll = Poll::new().unwrap();
+            poll.registry()
+                .register(&mut SourceFd(&eventfd), Token(0), Interest::READABLE)
+                .unwrap();
+            let mut events = Events::with_capacity(1024);
 
-    let mut ring = IoUring::builder().build(128).unwrap();
-    let submitter = ring.submitter();
-    submitter.register_files(&fds).unwrap();
-    submitter.register_eventfd(eventfd).unwrap();
+            let mut ring = IoUring::builder().setup_sqpoll(2000).build(128).unwrap();
+            let submitter = ring.submitter();
+            submitter.register_files(&fds).unwrap();
+            submitter.register_eventfd(eventfd).unwrap();
 
-    let buffers = unsafe { malloc(128 * BUF_SIZE) };
+            let buffers = unsafe { malloc(128 * BUF_SIZE) };
 
-    unsafe {
-        ring.submission()
-            .push(
-                &opcode::ProvideBuffers::new(buffers as _, BUF_SIZE as _, 128, 0, 0)
-                    .build()
-                    .user_data(3),
-            )
-            .unwrap();
+            unsafe {
+                ring.submission()
+                    .push(
+                        &opcode::ProvideBuffers::new(buffers as _, BUF_SIZE as _, 128, 0, 0)
+                            .build()
+                            .user_data(3),
+                    )
+                    .unwrap();
 
-        ring.submission().push(&prep_read(0)).unwrap();
-        ring.submission().push(&prep_read(1)).unwrap();
-        ring.submit().unwrap();
+                ring.submission().push(&prep_read(0)).unwrap();
+                ring.submission().push(&prep_read(1)).unwrap();
+                ring.submit().unwrap();
 
-        loop {
-            drop(poll.poll(&mut events, None));
-            let mut sq = ring.submission_shared();
-            for cqe in ring.completion_shared() {
-                let data = cqe.user_data();
-                match data {
-                    0 | 1 => {
-                        let buf = buffer_select(cqe.flags());
-                        if cqe.result() > 0 {
-                            let src = from_raw_parts(
-                                (buffers as *mut u8).add(buf.unwrap() as usize * BUF_SIZE),
-                                cqe.result() as usize,
-                            );
-                            let dst = from_raw_parts_mut(
-                                (buffers as *mut u8)
-                                    .add((buf.unwrap() as usize * BUF_SIZE) + PKT_SIZE),
-                                PKT_SIZE,
-                            );
-                            match match data {
-                                0 => tunnel.encapsulate(&src, dst),
-                                1 => tunnel.decapsulate(None, &src, dst),
-                                _ => unreachable!(),
-                            } {
-                                TunnResult::Done => (),
-                                TunnResult::Err(_) => (),
-                                TunnResult::WriteToNetwork(packet) => {
-                                    sq.push_multiple(&prep_write(
-                                        1,
-                                        buffers,
-                                        buf.unwrap(),
-                                        packet.len() as u32,
-                                    ))
-                                    .unwrap();
-                                    // FIXME: should flush, but would complicate buffer mgmt
+                loop {
+                    drop(poll.poll(&mut events, None));
+                    let mut sq = ring.submission_shared();
+                    for cqe in ring.completion_shared() {
+                        let data = cqe.user_data();
+                        match data {
+                            0 | 1 => {
+                                let buf = buffer_select(cqe.flags());
+                                if cqe.result() > 0 {
+                                    let src = from_raw_parts(
+                                        (buffers as *mut u8).add(buf.unwrap() as usize * BUF_SIZE),
+                                        cqe.result() as usize,
+                                    );
+                                    let dst = from_raw_parts_mut(
+                                        (buffers as *mut u8)
+                                            .add((buf.unwrap() as usize * BUF_SIZE) + PKT_SIZE),
+                                        PKT_SIZE,
+                                    );
+                                    match match data {
+                                        0 => tunnel.lock().unwrap().encapsulate(&src, dst),
+                                        1 => tunnel.lock().unwrap().decapsulate(None, &src, dst),
+                                        _ => unreachable!(),
+                                    } {
+                                        TunnResult::Done => (),
+                                        TunnResult::Err(_) => (),
+                                        TunnResult::WriteToNetwork(packet) => {
+                                            sq.push_multiple(&prep_write(
+                                                1,
+                                                buffers,
+                                                buf.unwrap(),
+                                                packet.len() as u32,
+                                            ))
+                                            .unwrap();
+                                        }
+                                        TunnResult::WriteToTunnel(packet) => {
+                                            sq.push_multiple(&prep_write(
+                                                0,
+                                                buffers,
+                                                buf.unwrap(),
+                                                packet.len() as u32,
+                                            ))
+                                            .unwrap();
+                                        }
+                                    }
+                                } else if let Some(buf) = buf {
+                                    sq.push(&prep_buffer(buffers, buf)).unwrap();
                                 }
-                                TunnResult::WriteToTunnel(packet) => {
-                                    sq.push_multiple(&prep_write(
-                                        0,
-                                        buffers,
-                                        buf.unwrap(),
-                                        packet.len() as u32,
-                                    ))
-                                    .unwrap();
-                                }
+                                sq.push(&prep_read(data as u32)).unwrap();
                             }
-                        } else if let Some(buf) = buf {
-                            sq.push(&prep_buffer(buffers, buf)).unwrap();
+                            _ => {}
                         }
-                        sq.push(&prep_read(data as u32)).unwrap();
                     }
-                    _ => {}
+                    sq.sync();
+                    ring.submit().unwrap();
                 }
             }
-            sq.sync();
-            ring.submit().unwrap();
-        }
+        });
     }
+    loop {}
 }
